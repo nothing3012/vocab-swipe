467c7f3ae562db081245243fd7cb4741e6b9c0dc
nothing3012 <116062268+nothing3012@users.noreply.github.com>
2026-09-13 18:04:22 +0800

    Publish vocabulary swipe app

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE = "vocab-swipe-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./vocab.json"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
