46f3a27e03c5e0801de72c9df759cb752a2fa9da
nothing3012 <116062268+nothing3012@users.noreply.github.com>
2026-09-14 15:21:32 +0800

    Refresh cached sync app

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "vocab-swipe-v3";
+const CACHE = "vocab-swipe-v4";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./vocab.json"];
 
 self.addEventListener("install", event => {
