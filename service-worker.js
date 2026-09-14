d6b65d4829e55c57a59b71d7bcb1ae5cdf849190
nothing3012 <116062268+nothing3012@users.noreply.github.com>
2026-09-14 11:39:31 +0800

    Add scene and 100-word group controls

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "vocab-swipe-v1";
+const CACHE = "vocab-swipe-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./vocab.json"];
 
 self.addEventListener("install", event => {
